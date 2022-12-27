996c3231ba4a2606eb3bdc1708b465490cb35538
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-27 22:26:40 +0100

    moved Navigation to section folder, added open close, added colors

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,10 @@ import About from "../../sections/About/About";
 import Projects from "../../sections/Projects/Projects";
 import Welcome from "../../sections/Welcome/Welcome";
 import Work from "../../sections/Work/Work";
-import Headlines from "../../components/Headlines/Headlines";
-import Navigation from "../../components/Navigation/Navigation";
 
 function Home() {
   return (
     <>
-      <Navigation />
       <Welcome />
       <About />
       <Projects />
--- src/sections/Navigation/Navigation.jsx
+++ src/sections/Navigation/Navigation.jsx
@@ -1,6 +1,40 @@
-import React from "react";
-import styles from "./navigation.module.css";
+import React, { useState } from "react";
+import styles from "./Navigation.module.css";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
-export default function Navigation() {
-  return <div>Navigation</div>;
+function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className={styles.container}>
+        <div className={styles.logo}>Logo</div>
+        <div
+          className={styles.burger_menu}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? (
+            <MdClose className={styles.burger_logo} />
+          ) : (
+            <FiMenu className={styles.burger_logo} />
+          )}
+        </div>
+      </div>
+
+      <ul className={`${styles.menu} ${isMenuOpen ? styles.open : ""}`}>
+        <li>
+          <p onClick={() => setIsMenuOpen(!isMenuOpen)}>Home</p>
+        </li>
+        <li>
+          <p onClick={() => setIsMenuOpen(!isMenuOpen)}>About</p>
+        </li>
+        <li>
+          <p onClick={() => setIsMenuOpen(!isMenuOpen)}>Contact</p>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
+export default Navigation;
