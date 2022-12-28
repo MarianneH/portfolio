8e3d404ab4524c7d82567c7bdcd9b70387327569
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-28 15:17:17 +0100

    finished welcome section

--- src/sections/Welcome/Welcome.jsx
+++ src/sections/Welcome/Welcome.jsx
@@ -4,11 +4,15 @@ import styles from "./welcome.module.css";
 function Welcome() {
   return (
     <div className={styles.container}>
-      <div>Hi there &#128075;, my name is</div>
-      <div>Marianne Helbig</div>
-      <div>I'm a Junior Web Developer building things for the web</div>
+      <div className={styles.welcome_text}>
+        <p className={styles.salutation}>Hi there &#128075;, my name is</p>
+        <h1 className={styles.name}>Marianne Helbig.</h1>
+        <h2 className={styles.main_tag}>
+          I'm a <span>Junior Web Developer</span> building things for the web
+        </h2>
+      </div>
 
-      <Button button_text="Get in touch" />
+      <Button button_text="Get in touch!" />
     </div>
   );
 }
