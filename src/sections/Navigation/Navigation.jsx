3ece862c2dae3fbc7a15f58bf7da12eab4c9319d
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-28 12:50:14 +0100

    made navigation responsive

--- src/sections/Navigation/Navigation.jsx
+++ src/sections/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import styles from "./Navigation.module.css";
 import { MdClose } from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
 import Button from "../../components/Button/Button";
-import Logo from "../../assets/logoM.svg";
+import Logo from "../../assets/Logo/Logo";
 
 function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,7 +11,7 @@ function Navigation() {
   return (
     <nav>
       <div className={styles.container}>
-        {/* <Logo className={styles.logo} /> */}
+        <Logo className={styles.logo} />
         <div
           className={styles.burger_menu}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -50,7 +50,7 @@ function Navigation() {
             </p>
           </li>
         </ul>
-        <Button button_text="Get my Resume" />
+        <Button className={styles.button} button_text="Get my Resume" />
       </div>
     </nav>
   );
