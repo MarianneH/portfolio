d57009759d64479cb3945fb8242cf05d99402e7b
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-29 21:57:12 +0100

    made shown text dynamic using index of element

--- src/components/WorkExperiences/WorkExperiences.jsx
+++ src/components/WorkExperiences/WorkExperiences.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./WorkExperiences.module.css";
 import workexp from "./WorkExp_object.json";
 
 function WorkExperiences() {
   const [experiences, setExperiences] = useState(workexp);
   const [selected, setSelected] = useState(workexp.length - 1);
+  const [text, setText] = useState(null);
 
   function handleClick(index) {
-    console.log(index);
+    setSelected(index);
   }
+
+  useEffect(() => {
+    setText(experiences[selected].description);
+  }, [selected, experiences]);
   return (
-    <div className={styles.exp_container}>
-      {experiences.map((el, index) => {
-        return (
-          <p
-            key={index}
-            className={styles.exp_name}
-            onClick={(event) => handleClick(index)}
-          >
-            {el.name}
-          </p>
-        );
-      })}
+    <div className={styles.container}>
+      <div className={styles.exp_container}>
+        {experiences.map((el, index) => {
+          return (
+            <p
+              key={index}
+              className={styles.exp_name}
+              onClick={(event) => handleClick(index)}
+            >
+              {el.name}
+            </p>
+          );
+        })}
+      </div>
+      <p className={styles.description}>{text}</p>
     </div>
   );
 }
