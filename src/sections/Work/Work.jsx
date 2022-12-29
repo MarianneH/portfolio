0ca8b27585014bc9f0b0ade4dea3519ed39fc25c
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-29 10:12:40 +0100

    changed font family to Roboto

--- src/sections/Work/Work.jsx
+++ src/sections/Work/Work.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Headlines from "../../components/Headlines/Headlines";
 import styles from "./work.module.css";
 
 export default function Work() {
-  return <div>Work</div>;
+  return (
+    <div>
+      <Headlines headline_text="Work Experience" />
+    </div>
+  );
 }
