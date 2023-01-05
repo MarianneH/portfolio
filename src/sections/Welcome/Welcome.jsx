4871efdc3e4fc27e990d9f2844b269c3e9393ea0
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-05 22:35:13 +0100

    adaptions to Projects Section, removing of hidden scrollable part in navigation

--- src/sections/Welcome/Welcome.jsx
+++ src/sections/Welcome/Welcome.jsx
@@ -8,7 +8,7 @@ function Welcome() {
         <p className={styles.salutation}>Hi there &#128075;, my name is</p>
         <h1 className={styles.name}>Marianne Helbig.</h1>
         <h2 className={styles.main_tag}>
-          I'm a <span>Junior Web Developer</span> building things for the web
+          I'm a <span>Junior Web Developer</span> building things for the web.
         </h2>
       </div>
 
