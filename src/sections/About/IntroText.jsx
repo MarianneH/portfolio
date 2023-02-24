63ba19d5abdc82e44076403b05bf92c63a0d7d84
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-24 23:33:29 +0100

    add seperate file for text of About section

---
+++ src/sections/About/IntroText.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function IntroText() {
+  return (
+    <div>
+      <p>
+        Hi! I am Marianne Helbig and I enjoy learning and building new things.
+        From September 2022 - February 2023 I was enrolled in the the 20 week
+        Web Developer Program at the{" "}
+        <a
+          className="highlight"
+          href="https://www.wildcodeschool.com/en-GB"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Wild Code School
+        </a>{" "}
+        and I'm now on the look out for my first full-time position as a web
+        developer.
+      </p>
+      <p>
+        Previously I've been working as a <span>Senior Product Manager</span> at{" "}
+        <a
+          className="highlight"
+          href="https://www.misterspex.de/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mister Spex
+        </a>{" "}
+        and am now seeking the opportunity to switch careers. I am a supportive
+        and enthusiastic team player with the willingness to contribute quickly,
+        learn about new technologies and with great communication, time- and
+        project management skills.
+      </p>
+    </div>
+  );
+}
+
+export default IntroText;
