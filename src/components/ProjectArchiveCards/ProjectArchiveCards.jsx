8cbf2585d9298a7ac16159d86aba65045d286a8a
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-26 19:24:06 +0100

    add styling of archive component

--- src/components/ProjectArchiveCards/ProjectArchiveCards.jsx
+++ src/components/ProjectArchiveCards/ProjectArchiveCards.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import Tags from "../Tags/Tags";
 import styles from "./ProjectArchiveCards.module.css";
 import { SlFolder } from "react-icons/sl";
+import { FaGithub } from "react-icons/fa";
 
 function ProjectArchiveCards({ archived }) {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.upper}>
-        <SlFolder />
+        <SlFolder className={styles.folder} />
+        <a href={archived.github} target="_blank" rel="noopener noreferrer">
+          <FaGithub className={styles.socials} />
+          <span className={styles.visible_hidden}>GitHub</span>
+        </a>
       </div>
       <div className={styles.project_details}>
-        <h3></h3>
-        <p></p>
-        <Tags tags={[]} />
+        <h3>{archived.name}</h3>
+        <p>{archived.short_descr}</p>
+        <Tags tags={archived.tags} />
       </div>
     </div>
   );
--- src/sections/ProjectArchive/ProjectArchive.jsx
+++ src/sections/ProjectArchive/ProjectArchive.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ProjectArchiveCards from "../../components/ProjectArchiveCards/ProjectArchiveCards";
+import projects from "../../components/ProjectCards/Projects_object.json";
+import styles from "./ProjectArchive.module.css";
 
 function ProjectArchive() {
   return (
-    <div>
-      <ProjectArchiveCards archive={[]} />
+    <div className={styles.container}>
+      {projects.slice(3).map((el, id) => {
+        return <ProjectArchiveCards archived={el} />;
+      })}
     </div>
   );
 }
