a4044b3f0be1307ff402fefe8f5807e14865aac8
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-26 18:23:13 +0100

    add files for archive

---
+++ src/components/ProjectArchiveCards/ProjectArchiveCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ProjectArchiveCards.module.css";
+
+function ProjectArchiveCards() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default ProjectArchiveCards;
