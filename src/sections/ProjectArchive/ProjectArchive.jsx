b471f21e298feba523261f13138a17dab83acdad
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-27 13:10:32 +0100

    finish styling of projectarchivecards

--- src/sections/ProjectArchive/ProjectArchive.jsx
+++ src/sections/ProjectArchive/ProjectArchive.jsx
@@ -5,7 +5,9 @@ import styles from "./ProjectArchive.module.css";
 
 function ProjectArchive() {
   return (
-    <div>
+    <div className={styles.container}>
+      <p>view the archive</p>
+      <h2>More Noteworthy Projects</h2>
       <div className={styles.card_container}>
         {projects.slice(3).map((el, id) => {
           return <ProjectArchiveCards archived={el} />;
